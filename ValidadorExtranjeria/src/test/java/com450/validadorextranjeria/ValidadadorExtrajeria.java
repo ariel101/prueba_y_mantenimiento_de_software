2d6b2e2408cce56bf9aa97ca117d3087e9ccad60
ariel cayo <ariel.cayo.vargas@gmail.com>
2024-08-02 23:53:24 -0400

    rest

--- ValidadorExtranjeria/src/test/java/com450/validadorextranjeria/ValidadadorExtrajeria.java
+++
@@ -1,9 +0,0 @@
-package com450.validadorextranjeria;
-
-public class ValidadadorExtrajeria {
-    public boolean validar(String number) {
-        if (number==null) return false;
-        return number.length()==10 && number.contains("E-");
-
-    }
-}
